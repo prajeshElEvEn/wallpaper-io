dcfb18424ea1a124ddf54d8cc7320f0e4ac1265e
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-08-12 05:47:09 +0530

    - edited profile page

--- src/account/Profile.js
+++ src/account/Profile.js
@@ -40,20 +40,22 @@ const Profile = () => {
                     <div className='dash-right'>
                         <div className='top-bar'>
                             <div className='username'>
-                                @username
+                                <span>@</span>username
                             </div>
-                            <div className='downloads'>
-                                <span>1000</span>
-                                <svg width="24" height="24" fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="m12 16 4-5h-3V4h-2v7H8l4 5Z"></path>
-                                    <path d="M20 18H4v-7H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-2v7Z"></path>
-                                </svg>
-                            </div>
-                            <div className='likes'>
-                                <span>1000</span>
-                                <svg width="24" height="24" fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M20.206 4.793a5.938 5.938 0 0 0-4.21-1.754 5.9 5.9 0 0 0-3.995 1.558 5.904 5.904 0 0 0-6.279-1.1 5.942 5.942 0 0 0-1.93 1.3c-2.354 2.363-2.353 6.06.001 8.412L12 21.416l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416Z"></path>
-                                </svg>
+                            <div className='numbers'>
+                                <div className='downloads'>
+                                    <span>1000</span>
+                                    <svg width="24" height="24" fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="m12 16 4-5h-3V4h-2v7H8l4 5Z"></path>
+                                        <path d="M20 18H4v-7H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-2v7Z"></path>
+                                    </svg>
+                                </div>
+                                <div className='likes'>
+                                    <span>1,000,000</span>
+                                    <svg width="24" height="24" fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M20.206 4.793a5.938 5.938 0 0 0-4.21-1.754 5.9 5.9 0 0 0-3.995 1.558 5.904 5.904 0 0 0-6.279-1.1 5.942 5.942 0 0 0-1.93 1.3c-2.354 2.363-2.353 6.06.001 8.412L12 21.416l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416Z"></path>
+                                    </svg>
+                                </div>
                             </div>
                         </div>
                         <div className='bio-container'>
